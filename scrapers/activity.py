39c96717c3acf5023d5c945aed66618cfbbe3c9c
sj <sjcomeau@wpi.edu>
2020-04-13 19:38:28 -0400

    adding a better constructro for activity

--- scrapers/activity.py
+++ scrapers/activity.py
@@ -17,7 +17,17 @@ class Activity:
     def __init__(self):
         pass
 
+    def __init__(self, name, address, hours, avg_visitor_review, avg_time_spent, photo_location):
+        self.name = name,
+        self.address = address
+        for hour,day in zip(hours,self.hours.keys()):
+            self.hours[day] = hour
+        self.avg_visitor_review = avg_visitor_review
+        self.avg_time_spent = avg_time_spent
+        self.photo_location = photo_location
+
     # pass in top reviews
-    def get_tags(self, reviews):
+    # TODO
+    def set_tags(self, reviews):
         for review in reviews:
             pass
