9d1b21761862bf7a750665556c83d17b15d3b411
sj <sjcomeau@wpi.edu>
2020-04-13 20:23:21 -0400

    100 result for each one

--- scrapers/scraper_yelp.py
+++ scrapers/scraper_yelp.py
@@ -15,37 +15,38 @@ class Yelp:
         pass
 
 
-    def build_url(self, city, state):
-        return "https://yelp.com/search?find_desc=things%20to%20do&find_loc="+city+"%2C%20"+state 
+    def build_url(self, city, state, start=0):
+        return "https://yelp.com/search?find_desc=things%20to%20do&find_loc="+city+"%2C%20"+state+"&start="+str(start)
 
     
-    def scrape(self, city, state):
+    def scrape(self, city, state, num_results=100):
         activities = []
 
-        url = self.build_url(city, state)
+        for block in range(int(num_results/10)):
+            url = self.build_url(city, state, start=block*10)
 
-        response = requests.get(url)
+            response = requests.get(url)
 
-        if response.status_code == 200:
-            # success
-            parser = html.fromstring(response.text)
-            listing = parser.xpath("//li[@class='regular-search-result']")
-            dirty_json = parser.xpath("//script[contains(@data-hypernova-key,'yelp_main__SearchApp')]//text()")
-            
-            # clean and load json
-            cleaned_json = dirty_json[0].replace("<!--", "").replace("-->", "").strip()
-            loaded_json = json.loads(cleaned_json)
+            if response.status_code == 200:
+                # success
+                parser = html.fromstring(response.text)
+                listing = parser.xpath("//li[@class='regular-search-result']")
+                dirty_json = parser.xpath("//script[contains(@data-hypernova-key,'yelp_main__SearchApp')]//text()")
+                
+                # clean and load json
+                cleaned_json = dirty_json[0].replace("<!--", "").replace("-->", "").strip()
+                loaded_json = json.loads(cleaned_json)
 
-            # get search results
-            search_results = loaded_json["searchPageProps"]["searchResultsProps"]["searchResults"]
+                # get search results
+                search_results = loaded_json["searchPageProps"]["searchResultsProps"]["searchResults"]
 
-            # loop through search results
-            for results in search_results:
-                result = results.get('searchResultBusiness')
+                # loop through search results
+                for results in search_results:
+                    result = results.get('searchResultBusiness')
 
-                # ignore ads and other fillers
-                if result:
-                    a = Activity(result['name'], result['formattedAddress'], result['rating'], None, None, "Yelp")
-                    activities.append(a)
+                    # ignore ads and other fillers
+                    if result:
+                        a = Activity(result['name'], result['formattedAddress'], result['rating'], None, None, "Yelp")
+                        activities.append(a)
 
         return activities
