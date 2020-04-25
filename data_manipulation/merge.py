af086b16c3ad6a81b5c6922d3bf929b1a59b02bf
sj <sjcomeau@wpi.edu>
2020-04-24 21:41:50 -0400

    merging done and first round executed

--- data_manipulation/merge.py
+++ data_manipulation/merge.py
@@ -3,75 +3,118 @@ Author:        Samantha
 Last modified: 4.24.2020 by sjc
 Status:        In progress
 
-This merges the results from the platforms in the data directory and writes the results to an inverte index 
+This merges the results from the platforms in the data directory
 
-TODO modularize the merge function
 TODO update with time_spent when those are integrated
 '''
 
 import os
 import json
-
 import argparse
 
 
-'''
-merge the results of the files
-'''
-def merge_results():
-# all data repository
-    all_data = {}
+import sys
+sys.path.insert(1, "../scrapers")
+from activity import Activity
 
-    # Walk directory
-    for root,dir,files in os.walk("../data"):
-        if root != "../data":
-            # get the files for each location
-            for file in files:
-                location = file.strip(".json")
-                if location not in all_data.keys():
-                    all_data[location] = {}
-                with open(os.path.abspath(root+"/"+file)) as activity_file:
-                    activities = json.load(activity_file)
-                    for activity in activities:
-                        if activity["name"] not in all_data[location]:
-                            all_data[location][activity.pop("name",None)] = activity
-                        else:
-                            ## handle duplicates
-                            name = activity.pop("name", None)
-                            duplicate_activity = activity
+class Merger:
 
-                            # use more detailed address
-                            duplicate_activity["address"] = all_data[location][name]["address"] if len(all_data[location][name]["address"]) > len(activity["address"]) else activity["address"]
-                            # average the visitor reviews
-                            duplicate_activity["avg_visitor_review"] = (float(activity["avg_visitor_review"]) + float(all_data[location][name]["avg_visitor_review"])) / 2
-                            # TODO time spent
-                            ### default to the second source
-                            # list of photos
-                            duplicate_activity["photo_location"] = [all_data[location][name]["photo_location"], activity["photo_location"]]
-                            # combine tags lists
-                            duplicate_activity["tags"] = []
-                            for tag in activity["tags"]:
-                                duplicate_activity["tags"].append(tag)
-                            for tag2 in all_data[location][name]["tags"]:
-                                if tag2 not in duplicate_activity["tags"]:
-                                    duplicate_activity["tags"].append(tag2)
-                            # list of sources
-                            duplicate_activity["source"] = [all_data[location][name]["source"], activity["source"]]
+    def __init__(self):
+        pass
 
-                            # replace the old entry
-                            all_data[location][name] = duplicate_activity
+    def merge_location(self, city, state):
+        # all data repository
+        merged_activities = {}
 
-    return all_data
+        # Walk directory
+        for root,dir,files in os.walk("../data"):
+            if root != "../data" and root != "../data/Merged" and root != "../data/Photographs":
+                # get the files for each location
+                for file in files:
+                    location = file.strip(".json")
+                    if location == str(city+state):
+                        with open(os.path.abspath(root+"/"+file)) as activity_file:
+                            activities = json.load(activity_file)
+                            for activity in activities:
+                                actobj = Activity(activity["name"], activity["address"], activity["avg_visitor_review"], activity["avg_time_spent"], activity["photo_location"], activity["source"], reviews=[], tags=activity["tags"])
+
+                                # fix lists
+                                actobj.tags = list(actobj.tags)
+                                if '[' in actobj.tags: actobj.tags.remove('[')
+                                if ']' in actobj.tags: actobj.tags.remove(']')
+
+                                if actobj.name not in merged_activities.keys():
+                                    # make lists of what would have become lists for consistency
+                                    actobj.source = [actobj.source]
+                                    actobj.photo_location = [actobj.photo_location]
+
+                                    merged_activities[actobj.name] = actobj
+                                    actobj = 0
+                                else:
+                                    ## handle duplicates
+                                    # use more detailed address
+                                    actobj.address = merged_activities[actobj.name].address if len(merged_activities[actobj.name].address) > len(actobj.address) else actobj.address
+
+                                    # average the visitor reviews
+                                    actobj.avg_visitor_review = (float(actobj.avg_visitor_review) + float(merged_activities[actobj.name].avg_visitor_review)) / 2
+
+                                    # TODO time spent
+                                    ### default to the second source
+                                    
+                                    # list of photos
+                                    actobj.photo_location = [merged_activities[actobj.name].photo_location, actobj.photo_location]
+
+                                    # combine tags lists
+                                    for tag2 in merged_activities[actobj.name].tags:
+                                        if tag2 not in actobj.tags:
+                                            actobj.tags.append(tag2)
+
+                                    # list of sources
+                                    actobj.source = [merged_activities[actobj.name].source, actobj.source]
+
+                                    # replace the old entry
+                                    merged_activities[actobj.name] = actobj
+                                    actobj = 0
+
+        # return to standard formattign which is a list of activites
+        standard = []
+        for activity in merged_activities.keys():
+            # add
+            standard.append(merged_activities[activity])
+
+        return standard
+
+    def merge_all(self, list_of_locations):
+        all_activities = {}
+        for location in list_of_locations:
+            all_activities[str(location[0]+location[1])] = self.merge_location(location[0], location[1])
+        return all_activities
+
+    def export(self, city, state, activities):
+        with open("../data/Merged/"+city+state+".json", "w") as outfile:
+            outfile.write("[\n")
+            for activity in activities:
+                json.dump(activity.encode(), outfile, indent=1)
+                if activity != activities[-1]:
+                    outfile.write(",")
+            outfile.write("]\n")
 
 def main():
-    # argument parser
+    merger = Merger()
+
+    # parser
     parser = argparse.ArgumentParser()
-
-    # parser.add_argument("-l", "--locations", required=True, help="Locations.txt")
-
+    parser.add_argument("-l", "--locations", help="locations.txt file", required=True)
     args = parser.parse_args()
 
-    results = merge_results()
+    with open(args.locations, "r") as locations_file:
+        locations = locations_file.readlines()
+        locations = [(loc.strip('\n').split(',')[0],loc.strip('\n').split(',')[1]) for loc in locations]
+
+    # merge all current
+    all_activities = merger.merge_all(locations)
+    for location in locations:
+        merger.export(location[0], location[1], all_activities[str(location[0]+location[1])])
 
     
 main()
--- scrapers/activity.py
+++ scrapers/activity.py
@@ -1,15 +1,18 @@
 '''
 Author:        Samantha
 Last modified: 4.24.2020 by sjc
-Status:        Need tags function
+Status:        In progress
 
 This holds an activity and can translate it to a json string
+
+# TODO tagging function
 '''
 
 import json
 
 class Activity:
     # Not sure we need these static members..? - Eda
+    # this is so when people are writing scrapers they know what to store and what type / location is should be in
     name = ""
     address = ""
     avg_visitor_review = 0.0
@@ -21,7 +24,7 @@ class Activity:
     def __init__(self):
         pass
 
-    def __init__(self, name, address, avg_visitor_review, avg_time_spent, photo_location, source, reviews=[]):
+    def __init__(self, name, address, avg_visitor_review, avg_time_spent, photo_location, source, reviews=[], tags=[]):
         self.name = name
         self.address = address
         self.avg_visitor_review = avg_visitor_review
@@ -29,6 +32,7 @@ class Activity:
         self.photo_location = photo_location
         self.source = source
         self.reviews = reviews
+        self.tags = tags
 
     # pass in top reviews
     # TODO
@@ -42,6 +46,6 @@ class Activity:
                 "avg_visitor_review":self.avg_visitor_review,
                 "avg_time_spent":self.avg_time_spent,
                 "photo_location":self.photo_location,
-                "tags":str(self.tags),
+                "tags":self.tags,
                 "source":self.source,
                 "reviews":self.reviews}
